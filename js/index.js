b455694d38f405d506283033f9b39271619a2f35
Tadesse Jemal <tadessejemal63@gmail.com>
2022-08-01 22:18:04 +0300

    Add css and js on Awosome books

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const bookTitle = document.querySelector('book-title');
+const bookAuthor = document.querySelector('book-author');
+const bookForm = document.getElementsByTagName('form')[0];
+const awosomeBook = document.querySelector('book-store');
+const bookBtn = document.getElementById('add-btn');
