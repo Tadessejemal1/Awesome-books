f14fda6e936685d8a1a16e3db7aaedb630de23f0
Peter1907 <peter.nady.1408@gmail.com>
2022-08-01 21:57:05 +0200

    Add book-list with JS

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
 const bookTitle = document.querySelector('book-title');
 const bookAuthor = document.querySelector('book-author');
 const bookForm = document.getElementsByTagName('form')[0];
-const awosomeBook = document.querySelector('book-store');
+const awosomeBook = document.querySelector('.book-store');
 const bookBtn = document.getElementById('add-btn');
+
+const bookList = [
+  {title: 'Atomic Habits',
+  author: 'James Clear',
+},{
+  title: 'Thinking fast and slow',
+  author: 'Dean burnette',
+},
+];
+
+
+for (let i = 0; i < bookList.length; i++) {
+  const p = document.createElement('p');
+  const p2 = document.createElement('p');
+  const btn = document.createElement('button');
+  btn.type = 'button';
+  btn.classList.add('remove-btn');
+  btn.textContent = 'remove';
+  const data = bookList[i];
+  p.textContent = data.title;
+  awosomeBook.appendChild(p);
+  p2.textContent = data.author;
+  awosomeBook.appendChild(p2);
+  awosomeBook.appendChild(btn);
+}
