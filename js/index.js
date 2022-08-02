78cd5dbe8438b1e0638023c86fb769c5c34fd1c8
Peter Beshara <106440905+Peter1907@users.noreply.github.com>
2022-08-02 14:32:10 +0200

    Fix eslint error

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/* eslint:disable:max-classes-per-file */
+eslint:disable:max-classes-per-file
 const bookTitle = document.querySelector('.book-title');
 const bookAuthor = document.querySelector('.book-author');
 const awosomeBook = document.querySelector('.book-store');
